8ad6e47225c17da802a1694648f7ed1d573cc366
schillidac <schillidac@gmail.com>
2017-10-24 17:24:31 +0200

    24-10-2017

--- 2 NOVIEMBRE/js.js
+++ 2 NOVIEMBRE/js.js
@@ -8,50 +8,53 @@ class Usuarios{
 
 	}
 
-	mostrarUsuarios(){
 
-		//creamos una fila y le añadimos un id para poder reemplazarlas o eliminarlas en siguientes ejecuciones
-		var fila = document.createElement("tr");
-		fila.setAttribute("class", "filas");
-		//creamos columna
-		var columna = document.createElement("td");
-		this.usuario = document.createTextNode(this.usuario);
-		columna.appendChild(this.usuario);
-		//añadimos las filas y las columnas a la tabla
-		document.getElementById("tabla").appendChild(fila);
-		fila.appendChild(columna);
+	generarTabla(contador){
 
-	}
-
-	mostrarProvincia(provincia){
-
-
-		if(this.provincia == provincia){
-
-			//creamos una fila y le añadimos un id para poder reemplazarlas o eliminarlas en siguientes ejecuciones
-			var fila = document.createElement("tr");
-			fila.setAttribute("class", "filas");
-			//creamos columnas
-			var columna1 = document.createElement("td");
-			var columna2 = document.createElement("td");
-			this.usuario = document.createTextNode(this.usuario);
-			columna1.appendChild(this.usuario);
-			this.provincia = document.createTextNode(this.provincia);
-			columna2.appendChild(this.provincia);
-			//añadimos las filas y las columnas a la tabla
-			document.getElementById("tabla").appendChild(fila);
-			fila.appendChild(columna1);
-			fila.appendChild(columna2);
-
+		if(parseInt(contador) == 0){
+			var tbody = document.createElement("tbody");
+			tbody.setAttribute("id", "table-body");
+			document.getElementById("tabla").appendChild(tbody);
 		}
 
+		var fila = document.createElement("tr");
+		fila.setAttribute("id", ""+contador+"")
+		fila.setAttribute("class", "fila");
+		//creamos columnas
+		var columna1 = document.createElement("td");
+		columna1.appendChild(document.createTextNode(this.usuario));
+		var columna2 = document.createElement("td");
+		columna2.appendChild(document.createTextNode(this.provincia));
+		var columna3 = document.createElement("td");
+		columna3.appendChild(document.createTextNode(this.cuota));
+		//añadimos las filas y las columnas a la tabla
+		document.getElementById("table-body").appendChild(fila);
+		fila.appendChild(columna1);
+		fila.appendChild(columna2);
+		fila.appendChild(columna3);
+
 	}
 
-	mostrarCuota(){
 
-		var columna = document.createElement("td");
-		this.cuota = document.createTextNode(this.cuota);
-		columna.appendChild(this.cuota);
+	mostrarUsuarios(contador){
+		
+		this.generarTabla(contador);
+
+	}
+
+	mostrarProvincia(contador, provincia){
+
+
+		if(this.provincia == provincia)
+			this.generarTabla(contador);
+
+
+	}
+
+	mostrarCuota(contador, cuota){
+
+		if(this.cuota < cuota)
+			this.generarTabla(contador);
 
 	}
 
@@ -62,7 +65,9 @@ class Usuarios{
 
 var texto = ["Laura;Santander;50" , "Álvaro;Castro;50" , 
 "Igor;Castro;60", "Ivan;Santander;40" , "Mónica;Zamora;30" , "Javi;Bilbao;30" , "David;Bilbao;50" , "José Luis;Bilbao;60"];
+
 var objetos = [];
+
 var contador = 0;
 
 for(var i = 0; i < texto.length; i++){
@@ -74,24 +79,26 @@ for(var i = 0; i < texto.length; i++){
 
 function mostrar(num){
 
-	if(parseInt(contador) > 0){
-		var eliminar = document.getElementById("fila");
-		document.getElementById("tabla").removeChild(eliminar[i]);
-	}
+
+	var tbody = document.getElementById("table-body");
+	tbody.parentNode.removeChild(tbody);
 
 
 	switch(num){
 
 		case 0:
 			for(var i = 0; i < objetos.length; i++)
-				objetos[i].mostrarUsuarios();	
+				objetos[i].mostrarUsuarios(i);	
 		break;
 		case 1:
 			var provincia = prompt("Indica una provincia");
 			for(var i = 0; i < objetos.length; i++)
-				objetos[i].mostrarProvincia(provincia);	
+				objetos[i].mostrarProvincia(i, provincia);	
 		break;
 		case 2:
+			var cuota = prompt("Indica una cuota");
+			for(var i = 0; i < objetos.length; i++)
+				objetos[i].mostrarCuota(i, cuota);
 
 	}
 
