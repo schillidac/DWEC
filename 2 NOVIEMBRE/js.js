3031264559acf0e97476a9abae2bb7367ebb409f
schillidac <schillidac@gmail.com>
2017-10-24 14:50:03 +0200

    v2

--- 2 NOVIEMBRE/js.js
+++ 2 NOVIEMBRE/js.js
@@ -1,48 +1,110 @@
-var texto = ["Laura;Santander;50" , "Álvaro;Castro;50" , 
-"Igor;Castro;60", "Ivan;Santander;40" , "Mónica;Zamora;30" , "Javi;Bilbao;30" , "David;Bilbao;50" , "José Luis;Bilbao;60"];
+class Usuarios{
 
-;
+	constructor(usuario, provincia, cuota){
 
-var contador = 0;
-var padre = document.getElementById("tabla");
+		this.usuario = usuario;
+		this.provincia = provincia;
+		this.cuota = cuota;
 
+	}
 
-
-
-
-function mostrar(num){
-
-	for(var i = 0;i<texto.length;i++){
-
-		if(parseInt(contador) > 0){
-
-			//Si ya se ha ejecutado el script al menos una vez, eliminará los elementos que se crearon anteriormente.
-			var eliminar = document.getElementById(""+i+"");
-			padre.removeChild(eliminar);
-
-		}
-
-
-		//extraemos los usuarios, provincias o cuotas, según el parámetro que entre en la llamada a la función
-		var usuarios = texto[i].split(";");
-		var usuario = document.createTextNode(usuarios[num]);
+	mostrarUsuarios(){
 
 		//creamos una fila y le añadimos un id para poder reemplazarlas o eliminarlas en siguientes ejecuciones
 		var fila = document.createElement("tr");
-		fila.setAttribute("id", ""+i+"");
-
-		//creamos una columna
+		fila.setAttribute("class", "filas");
+		//creamos columna
 		var columna = document.createElement("td");
-
+		this.usuario = document.createTextNode(this.usuario);
+		columna.appendChild(this.usuario);
 		//añadimos las filas y las columnas a la tabla
-		padre.appendChild(fila);
-		columna.appendChild(usuario);
+		document.getElementById("tabla").appendChild(fila);
 		fila.appendChild(columna);
 
 	}
 
-	parseInt(contador++);
+	mostrarProvincia(provincia){
+
+
+		if(this.provincia == provincia){
+
+			//creamos una fila y le añadimos un id para poder reemplazarlas o eliminarlas en siguientes ejecuciones
+			var fila = document.createElement("tr");
+			fila.setAttribute("class", "filas");
+			//creamos columnas
+			var columna1 = document.createElement("td");
+			var columna2 = document.createElement("td");
+			this.usuario = document.createTextNode(this.usuario);
+			columna1.appendChild(this.usuario);
+			this.provincia = document.createTextNode(this.provincia);
+			columna2.appendChild(this.provincia);
+			//añadimos las filas y las columnas a la tabla
+			document.getElementById("tabla").appendChild(fila);
+			fila.appendChild(columna1);
+			fila.appendChild(columna2);
+
+		}
+
+	}
+
+	mostrarCuota(){
+
+		var columna = document.createElement("td");
+		this.cuota = document.createTextNode(this.cuota);
+		columna.appendChild(this.cuota);
+
+	}
+
 }
+
+
+
+
+var texto = ["Laura;Santander;50" , "Álvaro;Castro;50" , 
+"Igor;Castro;60", "Ivan;Santander;40" , "Mónica;Zamora;30" , "Javi;Bilbao;30" , "David;Bilbao;50" , "José Luis;Bilbao;60"];
+var objetos = [];
+var contador = 0;
+
+for(var i = 0; i < texto.length; i++){
+
+	var usuarios = texto[i].split(";");
+	objetos[i] = new Usuarios(usuarios[0],usuarios[1],usuarios[2]);
+	
+}
+
+function mostrar(num){
+
+	if(parseInt(contador) > 0){
+		var eliminar = document.getElementById("fila");
+		document.getElementById("tabla").removeChild(eliminar[i]);
+	}
+
+
+	switch(num){
+
+		case 0:
+			for(var i = 0; i < objetos.length; i++)
+				objetos[i].mostrarUsuarios();	
+		break;
+		case 1:
+			var provincia = prompt("Indica una provincia");
+			for(var i = 0; i < objetos.length; i++)
+				objetos[i].mostrarProvincia(provincia);	
+		break;
+		case 2:
+
+	}
+
+	parseInt(contador++);
+
+}
+
+
+
+
+
+
+
 
 
 
