4ed8f5ada97c5b5090148bcd8ef6a794846b26f0
schillidac <schillidac@gmail.com>
2017-10-24 13:04:37 +0200

    v1.0 (2 NOVIEMBRE)
    mostrar por usuario finalizado

--- 2 NOVIEMBRE/js.js
+++ 2 NOVIEMBRE/js.js
@@ -1,43 +1,47 @@
 var texto = ["Laura;Santander;50" , "Álvaro;Castro;50" , 
 "Igor;Castro;60", "Ivan;Santander;40" , "Mónica;Zamora;30" , "Javi;Bilbao;30" , "David;Bilbao;50" , "José Luis;Bilbao;60"];
-var caja = document.getElementById("caja"); 
+
+;
+
+var contador = 0;
+var padre = document.getElementById("tabla");
+
+
+
+
 
 function mostrar(num){
 
-	
+	for(var i = 0;i<texto.length;i++){
 
-	switch(parseInt(num)){
-		case 0:
-			for(var i = 0; i<texto.length; i++){
+		if(parseInt(contador) > 0){
 
-				var usuario = texto[i].split(";");
-				caja.innerHTML += usuario[0] + "<br>";
+			//Si ya se ha ejecutado el script al menos una vez, eliminará los elementos que se crearon anteriormente.
+			var eliminar = document.getElementById(""+i+"");
+			padre.removeChild(eliminar);
 
-			}
-		break;
-		case 1:
-
-			var provincia = prompt("Dime una provincia");
-			
-
-			
-
-			for(var i = 0; i<texto.length; i++){
-
-				var provincias = texto[i].split(";");
-
-				if(provincia.toUpperCase() == provincias[1].toUpperCase())
-					alert(provincias[0]);
-
-					
-
-			}
+		}
 
 
+		//extraemos los usuarios, provincias o cuotas, según el parámetro que entre en la llamada a la función
+		var usuarios = texto[i].split(";");
+		var usuario = document.createTextNode(usuarios[num]);
+
+		//creamos una fila y le añadimos un id para poder reemplazarlas o eliminarlas en siguientes ejecuciones
+		var fila = document.createElement("tr");
+		fila.setAttribute("id", ""+i+"");
+
+		//creamos una columna
+		var columna = document.createElement("td");
+
+		//añadimos las filas y las columnas a la tabla
+		padre.appendChild(fila);
+		columna.appendChild(usuario);
+		fila.appendChild(columna);
 
 	}
 
-
+	parseInt(contador++);
 }
 
 
